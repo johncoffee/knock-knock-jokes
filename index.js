23d047ed7b91dd82d945e265a5d90f6d61f33edf
johncoffee <github2017@ragelse.dk>
2018-09-01 20:19:16 +0200

    golf golf

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-module.exports=()=>require("./jokes.json").sort(()=>-1+Math.random()*2).map(({name,answer})=>`
-Knock, knock.
+module.exports=()=>require("./jokes.json").sort(()=>-1+Math.random()*2).map(({name,answer})=>`Knock, knock.
 Who’s there?
 ${name}
 ${name} who?
